ccbbcc4f13e0f487ef82dbd29ab96b4f910173ff
Buck Baskin <mobile.wbaskin@gmail.com>
2017-04-11 13:54:52 -0400

    Begin using navigator API on page

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+console.log('Load nav.js');
+
+function setupGeolocation(navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        $("#latitude").html("" + position.coords.latitude);
+        $("#longitude").html("" + position.coords.longitude);
+        console.log(position.coords.latitude, position.coords.longitude);
+    });
+}
+
+if ("geolocation" in navigator) {
+    console.log("Run Geolocation Setup");
+    setupGeolocation(window.navigator);
+}
