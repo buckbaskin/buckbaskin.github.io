6f37b04d05ab1d4743e4e14edfb85997112baa82
Buck Baskin <mobile.wbaskin@gmail.com>
2017-04-11 15:17:08 -0400

    Setup a location watch

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,24 @@
 console.log('Load nav.js');
 
 function setupGeolocation(navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    function geoSuccess(position) {
         $("#latitude").html("" + position.coords.latitude);
         $("#longitude").html("" + position.coords.longitude);
         $("#commentary").html("The Geolocation Javascript Loaded.");
         console.log(position.coords.latitude, position.coords.longitude);
-    });
+    }
+
+    function geoError() {
+        $("#commentary").html("There was an error loading the Geolocation.");
+    }
+
+    var geo_options = {
+        enableHighAccuracy: true,
+        maximumAge: 0
+    };
+
+    // navigator.geolocation.getCurrentPosition(geoSuccess);
+    var watchID = navigator.geolocation.watchPosition(geoSuccess);
 }
 
 function setup() {
