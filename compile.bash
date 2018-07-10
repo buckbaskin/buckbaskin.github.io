207085c10fb8789e4c154fc7c1f05ea317424db6
Buck Baskin <mobile.wbaskin@gmail.com>
2018-07-10 00:03:00 -0700

    Update compilation to go across multiple files

--- compile.bash
+++ compile.bash
@@ -1,1 +1,7 @@
-pandoc -s -c css/site.css markdown/resume.md -o html/resume.html
+pandoc -s -c css/site.css -c css/index.css index.md -o index.html
+
+for filename in markdown/*md; do
+    echo $(basename $filename .md)
+    pandoc -s -c css/site.css -c css/$(basename $filename .md).css $filename\
+    -o $(basename $filename .md).html
+done
