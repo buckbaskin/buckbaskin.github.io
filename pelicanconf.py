4472d31d5a929b5d71905f7754f2d922bd2eddd0
Buck Baskin <mobile.wbaskin@gmail.com>
2018-07-16 22:51:44 -0700

    Generating the right index and resume pages (with custom print css for the resume)

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Buck Baskin'
 SITENAME = 'Building and Breaking Things'
+SITESUBTITLE = 'a blog by Buck Baskin'
 SITEURL = ''
 THEME = 'themes/simple'
 
@@ -21,14 +22,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),)
 
 DEFAULT_PAGINATION = False
 
