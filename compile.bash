b4d718fc7ba89fa090e876d850794b036269b2be
Buck Baskin <mobile.wbaskin@gmail.com>
2020-04-12 17:01:47 -0700

    New wheels

--- compile.bash
+++ compile.bash
@@ -1,14 +1,2 @@
-if [ -z "$(ls -A content/img/full/)"  ]; then
-    echo "No New Images to Compress"
-else
-    echo "Compressing images"
-    for filename in content/img/full/*.jpg; do
-        BASE=$(basename $filename .jpg)
-        convert $filename -sampling-factor 4:2:0 -strip -resize 1040x585\
-        -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg
-    done
-    echo "Done compressing images"
-fi
-echo "Generating the interwebs"
-pelican content -o blog -s pelicanconf.py
-echo "Done generating the interwebs"
+bash compile_images.bash
+bash compile_content.bash
---
+++ compile_images.bash
@@ -0,0 +1,17 @@
+if [ -z "$(ls -A content/img/full/)"  ]; then
+    echo "No New Images to Compress"
+else
+    echo "Compressing JPG"
+    for filename in content/img/full/*.jpg; do
+        BASE=$(basename $filename .jpg)
+        convert $filename -sampling-factor 4:2:0 -strip -resize 1040x585\
+        -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg
+    done
+    echo "Compressing PNG"
+    for filename in content/img/full/CAD*.png; do
+        BASE=$(basename $filename .png)
+        convert $filename -sampling-factor 4:2:0 -strip -resize 1040x585\
+        -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg
+    done
+    echo "Done compressing images"
+fi
