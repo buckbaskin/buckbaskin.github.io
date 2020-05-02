75a37a0e2e2404d9f16afc37e255a79493586fad
Buck Baskin <mobile.wbaskin@gmail.com>
2020-05-01 21:49:23 -0700

    Blog post for 4/27

--- parallel_compile_images.bash
+++ parallel_compile_images.bash
@@ -1,6 +1,8 @@
 if [ -z "$(ls -A content/img/full/)"  ]; then
     echo "No New Images to Compress"
 else
+    let "CORES_MINUS=$(nproc --all) - 1"
+    echo "Working with ${CORES_MINUS} cores"
     echo "Compressing JPG"
     for filename in content/img/full/*.jpg; do
         echo Compressing ${filename}
@@ -10,7 +12,7 @@ else
         -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg &
 
         num_children=`ps --no-headers -o pid --ppid=$$ | wc -w`
-        if [ $num_children -gt 4 ]
+        if [ $num_children -gt ${CORES_MINUS} ]
         then
             echo waiting for reduced job count
             wait
@@ -28,7 +30,7 @@ else
         -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg &
 
         num_children=`ps --no-headers -o pid --ppid=$$ | wc -w`
-        if [ $num_children -gt 4 ] 
+        if [ $num_children -gt ${CORES_MINUS} ] 
         then
             echo waiting for reduced job count
             wait
@@ -36,6 +38,24 @@ else
     done
     wait
     echo "Done with CAD PNG"
+
+    echo "Compressing Slice PNG"
+    for filename in content/img/full/*Slice*.png; do
+        echo Compressing ${filename}
+
+        BASE=$(basename $filename .png)
+        convert $filename -sampling-factor 4:2:0 -strip -resize 1040x585\
+        -quality 85 -interlace JPEG -colorspace sRGB content/img/$BASE.jpg &
+
+        num_children=`ps --no-headers -o pid --ppid=$$ | wc -w`
+        if [ $num_children -gt ${CORES_MINUS} ] 
+        then
+            echo waiting for reduced job count
+            wait
+        fi
+    done
+    wait
+    echo "Done with Slice PNG"
 
     echo "Done compressing images"
 fi
